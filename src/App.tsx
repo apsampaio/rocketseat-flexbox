bb65d45efce97a6023fe3ff50039e4ec36efca70
apsampaio <andre03.sampaio@gmail.com>
2020-10-30 11:13:49 -0300

    profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 import { GiKiwiBird } from "react-icons/gi";
-import { FiHome, FiBell, FiMail, FiMoreVertical } from "react-icons/fi";
+import {
+  FiHome,
+  FiBell,
+  FiMail,
+  FiMoreVertical,
+  FiMapPin,
+  FiLink,
+  FiClock,
+  FiSmile,
+  FiUser,
+  FiImage,
+} from "react-icons/fi";
 
 import "./styles.css";
 
@@ -62,6 +73,89 @@ function App() {
             <FiMoreVertical color="#667580" size={18} />
           </div>
         </div>
+      </div>
+      <div className="wrapper-content wrapper">
+        <aside className="profile">
+          <img
+            className="avatar"
+            src="https://avatars0.githubusercontent.com/u/51516616?s=460&u=9dceff67107fcc14afd0339af87e1fadeac4dfea&v=4"
+            alt="avatar"
+          />
+          <h1>André Sampaio</h1>
+          <span>@andore</span>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <ul className="list">
+            <li>
+              <FiMapPin size={18} color="#667580" /> Sorocaba, Brazil
+            </li>
+            <li>
+              <FiLink size={18} color="#667580" /> github.com/apsampaio
+            </li>
+            <li>
+              <FiClock size={18} color="#667580" /> Joined June 2007
+            </li>
+            <li>
+              <FiSmile size={18} color="#667580" /> Born the 3th of March 1997
+            </li>
+          </ul>
+          <div className="widget followers">
+            <strong>
+              <FiUser size={18} color="#667580" /> 73 followers that you know
+            </strong>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div className="widget images">
+            <strong>
+              <FiImage size={18} color="#667580" /> 266 photos and videos
+            </strong>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </aside>
+        <section className="timeline">timeline</section>
+        <aside className="widgets">
+          <div className="widget follow">
+            <div className="title">
+              <strong>Who to follow</strong>
+              <a href="">Refresh</a>
+              <a href="">View all</a>
+            </div>
+
+            <ul>
+              <li>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/51516616?s=460&u=9dceff67107fcc14afd0339af87e1fadeac4dfea&v=4"
+                  alt="avatar"
+                />
+                <div className="info">
+                  <strong>
+                    Spade <span>@spade_be</span>
+                  </strong>
+                  <button>Follow</button>
+                </div>
+                <a href="">x</a>
+              </li>
+            </ul>
+          </div>
+          <div className="widget trends"></div>
+        </aside>
       </div>
     </div>
   );
